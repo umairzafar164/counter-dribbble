290a07ca437cbd2dc456f54dd1daaa9a14396f5a
Umair Zafar <umairzafar164@gmail.com>
2021-05-13 03:47:00 +0500

    styling done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-
+import { MdKeyboardArrowRight,MdKeyboardArrowLeft } from 'react-icons/md';
 function Counter() {
   const [number, setNumber]=useState(0);
   const previous=()=>{setNumber(number-1)}
@@ -10,9 +10,9 @@ function Counter() {
 
     <div className='main-div' >
       <div className='small-div'>
-        <button onClick={previous} disabled={number===0}>-</button>
-        <h1>{number}</h1>
-        <button onClick={next}>+</button>
+        <button onClick={previous} style={{cursor:'pointer'}} disabled={number===0} className='button'><MdKeyboardArrowLeft className='arrow'/></button>
+        <span className='span'><h1 className='heading'>{number}</h1></span>
+        <button onClick={next} className='button' style={{cursor:'pointer'}}> <MdKeyboardArrowRight className='arrow'/></button>
       </div>
     </div>
   );
