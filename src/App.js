c4012adbfc4b780c0e5e5758bca4ce1cdd79243c
Umair Zafar <umairzafar164@gmail.com>
2021-05-18 12:55:50 +0500

    animation in process

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import { MdKeyboardArrowRight,MdKeyboardArrowLeft } from 'react-icons/md';
+import { useState } from "react";
+import "./App.css";
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 function Counter() {
-  const [number, setNumber]=useState(0);
-  const previous=()=>{setNumber(number-1)}
-  const next=()=>{setNumber(number+1)}
+  const [number, setNumber] = useState({ currentNumber: 0, animation: "none" });
+  const previous = () => {
+    setNumber({
+      currentNumber: number.currenNumber - 1,
+      animation: "animate-left",
+    });
+  };
+  const next = () => {
+    setNumber({
+      currentNumber: number.currentNumber + 1,
+      animation: "animate-right",
+    });
+  };
+  const resetAnimation = () => {
+    setNumber({
+      currentNumber: number.currentNumber,
+      animation: "none",
+    });
+  };
 
   return (
-
-    <div className='main-div' >
-      <div className='small-div'>
-        <button onClick={previous} style={{cursor:'pointer'}} disabled={number===0} className='button'><MdKeyboardArrowLeft className='arrow'/></button>
-        <span className='span'><h1 className='heading'>{number}</h1></span>
-        <button onClick={next} className='button' style={{cursor:'pointer'}}> <MdKeyboardArrowRight className='arrow'/></button>
+    <div className="main-div">
+      <div className="small-div">
+        <button
+          onClick={previous}
+          style={{ cursor: "pointer" }}
+          disabled={number.currentNumber === 0}
+          className="button"
+        >
+          <MdKeyboardArrowLeft className="arrow" />
+        </button>
+        <span className={`span ${number.animation}`} onAnimationEnd={resetAnimation}>
+          <h1 className="heading">{number.currentNumber}</h1>
+        </span>
+        <button onClick={next} className="button" style={{ cursor: "pointer" }}>
+          {" "}
+          <MdKeyboardArrowRight className="arrow" />
+        </button>
       </div>
     </div>
   );
 }
 
 export default Counter;
+
